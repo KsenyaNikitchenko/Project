1f4e589a57fae1c0b48c8e9605c94ba2c5b4fa1d
vadiksavintsev <113433935+vadiksavintsev@users.noreply.github.com>
2023-01-18 01:49:42 +0300

    Update forma.js

--- forma.js
+++ forma.js
@@ -66,3 +66,15 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     chekStorage();
 });
+$(document).ready(function() {
+    console.log( "ready!" );
+   $(".accordion h6:first").addClass("active");
+   $(".accordion p:not(:first)").hide();
+   $(".accordion h6").click(function () {
+     $(this).next("p").slideToggle("slow")
+       .siblings("p:visible").slideUp("slow");
+     $(this).toggleClass("active");
+     $(this).siblings("h6").removeClass("active");
+   });
+
+});
