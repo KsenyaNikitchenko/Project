68637087fc77628cf40ab142c66a06f1565dbb74
Nikol Gortez <117678971+NikolGortez@users.noreply.github.com>
2023-01-18 09:30:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,4 +57,19 @@ $(document).ready(function () {
         }
       ]
     });
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+  
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("accordion-active");
+        var panel = this.lastElementChild;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  });
   
